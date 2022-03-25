a43a71cef4dc12a1de7c3beef25fbdd9dc3cc149
GGpark1 <doempgg@gmail.com>
2022-03-25 22:56:13 +0900

    python module practice

---
+++ module_practice/modules/run.py
@@ -0,0 +1,9 @@
+import shapes
+
+x = float(input('원의 지름을 입력해 주세요: '))
+print(f"지름이 {x}인 원의 면적은 {shapes.area.circle(x)}입니다.")
+
+y = float(input('정사각형의 변의 길이를 입력해 주세요: '))
+print(f'변의 길이가 {y}인 정사각형의 면적은 {shapes.area.square(y)}입니다.')
+
+print(shapes.volume.cube(3))
